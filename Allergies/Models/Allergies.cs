f713acde0a882833b384f9e17e6bb73248769169
Ryan Bass <theryanbass@gmail.com>
2022-02-22 16:28:25 -0800

    alter logic to be mathmatical
    Co-authored-by: Eric Crudup <Cruduper@users.noreply.github.com>

--- Allergies/Models/Allergies.cs
+++ Allergies/Models/Allergies.cs
@@ -1,62 +1,29 @@
+using System; 
+using System.Text; 
 using System.Collections.Generic;
+
 
 namespace Allergies.Model 
 {
   public class Allergens
   {
-    public static string AllergenCheck( int allergenNum )
+    public static string AllergenCheck( double allergenNum )
     {
       List<string> aList = new List<string>();
-      
-      if (allergenNum > 127)
-      {
-        aList.Add("Cats");
-        allergenNum = allergenNum - 128;
-      }
 
-      if (allergenNum > 63)
-      {
-        aList.Add("Pollen");
-        allergenNum = allergenNum - 64;
+      List<string> allergens = new List<string>{"Eggs", "Peanuts", "Shellfish", "Strawberries", "Tomatoes", "Chocolate", "Pollen", "Cats"};
+
+
+      for ( int i = 7; i >= 0; i-- ){
+        if( allergenNum >= Math.Pow(2, i) ){
+          aList.Add(allergens[i]);
+          allergenNum -= Math.Pow(2, i);
+        }
       }
       
-      if (allergenNum > 31)
-      {
-        aList.Add("Chocolate");
-        allergenNum = allergenNum - 32;
-      }
-      
-      if (allergenNum > 15)
-      {
-        aList.Add("Tomatoes");
-        allergenNum = allergenNum - 16;
-      }
-
-      if (allergenNum > 7)
-      {
-        aList.Add("Strawberries");
-        allergenNum = allergenNum - 8;
-      }
-     
-      if (allergenNum > 3)
-      {
-        aList.Add("Shellfish");
-        allergenNum = allergenNum - 4;
-      }
-      if (allergenNum > 1)
-      {
-        aList.Add("Peanuts");
-        allergenNum = allergenNum - 2;
-      }
-      if ( allergenNum == 1 )
-      {
-        aList.Add("Eggs");
-      }
-
       string output = string.Join(" and ", aList.ToArray());
-
       return output;
-      //code here
+     
     }
   }
 
